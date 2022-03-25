dad7338cb8e5ecfe925d9c00e721d43c0d34a64f
Dinuka Gayashan <dinukagayashankasthuriarachchi@gmail.com>
2022-03-25 11:09:37 +0530

    develop

--- Mobile Application/ezdelivery/lib/main.dart
+++ Mobile Application/ezdelivery/lib/main.dart
@@ -1,14 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home:Center(
-        child:Text(
-          'Hello',
-          style:TextStyle(),
+  runApp(const ezDelivery());
+}
+
+class ezDelivery extends StatelessWidget {
+  const ezDelivery({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                width: 100.0,
+                color: Colors.red,
+                height: double.infinity,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.amber,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+                height: double.infinity,
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
