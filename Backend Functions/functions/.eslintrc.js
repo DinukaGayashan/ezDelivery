c6fbbecb1fc1bdcd292707ed99dc296e3f12b149
Dinuka Gayashan <dinukagayashankasthuriarachchi@gmail.com>
2022-04-16 18:46:54 +0530

    cloud functions added

---
+++ Backend Functions/functions/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "require-jsdoc": 0,
+    "quotes": ["error", "double"],
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+};
